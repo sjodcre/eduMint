33c1ddd4ed588d4427487b286bb4de7a0327614a
Your Name <you@example.com>
2025-02-10 19:03:14 +0800

    profile change

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import VideoFeed from "./components/VideoFeed";
 import { ScreenContext } from "./context/ScreenContext";
 import OnBoard from "./components/OnBoard";
 import { useContext } from "react";
-// import ProfilePage from "./pages/profile/profile-page";
 // import { useArweaveProvider } from "./context/ArweaveProvider";
 import { BottomNav } from "./components/Navbar";
 import Upload from "./pages/post/upload";
 // import { Market } from "./pages/market/market";
 import { Toaster } from "./components/ui/toaster";
-import ProfilePagePlaceholder from "./pages/profile/profile-demo";
+// import ProfilePagePlaceholder from "./pages/profile/profile-demo";
 import Course from "./pages/course";
+import ProfilePage from "./pages/profile/profile-page";
+import { useArweaveProvider } from "./context/ArweaveProvider";
 
 // export default function App() {
 //   const { currentScreen } = useContext(ScreenContext);
@@ -31,6 +32,7 @@ import Course from "./pages/course";
 
 export default function App() {
   const { currentScreen } = useContext(ScreenContext);
+  const {selectedUser} = useArweaveProvider()
 
   return (
     <div className="h-screen flex flex-col overflow-hidden">
@@ -39,7 +41,9 @@ export default function App() {
         {currentScreen === "videofeed" && <VideoFeed />}
         {currentScreen === "upload" && <Upload />}
         {currentScreen === "course" && <Course />}
-        {currentScreen === "profile" && <ProfilePagePlaceholder />}
+        {/* {currentScreen === "profile" && <ProfilePagePlaceholder />} */}
+        {currentScreen === "profile" && <ProfilePage user={selectedUser || null}/>}
+
       </div>
       <Toaster />
       <BottomNav />
